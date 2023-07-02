ef09d61c37db9667b742e9dd81b506cc927ac2bc
mickaelCocquempot <m.cocquempot@gmail.com>
2023-07-02 18:35:24 +0200

    start of todo app

---
+++ lib/2_application/app/basic_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import '../core/routes.dart';
+
+class BasicApp extends StatelessWidget {
+  const BasicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'To-Do app',
+      localizationsDelegates: const [
+        ...GlobalMaterialLocalizations.delegates,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      themeMode: ThemeMode.system,
+      theme: ThemeData.from(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepOrange, brightness: Brightness.light)),
+      darkTheme: ThemeData.from(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+      routerConfig: routes,
+    );
+  }
+}
